b0fddd2e48974ee5575ac3d1801de40e0bed1499
Chris Kettenbach <295833+hcabnettek@users.noreply.github.com>
2021-04-28 15:46:46 -0700

    added logging

--- data_server.ts
+++ data_server.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak@v7.3.0/mod.ts";
+import * as log from "https://deno.land/std@0.95.0/log/mod.ts";
 import { StarWarsPerson } from "./types.ts";
 
 const json = await Deno.readTextFile("star_wars_people.json");
@@ -9,11 +10,12 @@ const router = new Router();
 router
   .get('/products', (context: any) => {
     context.response.body = JSON.stringify(peopleArray, null, '  ');
+    log.info("Returning Star Wars People");
   })
 
 const app = new Application();
 app.use(router.routes());
 
-console.log("Listening for requests...")
+log.info("Listening for requests...")
 
 await app.listen({port: 8000 });
